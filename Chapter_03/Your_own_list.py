a80343bb95d9937e4d9988fde87af96bbe55ad20
lvazmecheng <262887731+lvazmecheng@users.noreply.github.com>
2026-04-17 13:27:15 +0500

    Finish exercise 3-3: Created a list of bikes and printed them in statements

---
+++ Chapter_03/Your_own_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+transportation = ['honda', 'yamaha', 'suzuki', 'vespa']
+print(f"I would like to own a {transportation[0]}.")
+print(f"I would like to own a {transportation[1]}.")
+print(f"I would like to own a {transportation[2]}.")
+print(f"I would like to own a {transportation[3]}.")
