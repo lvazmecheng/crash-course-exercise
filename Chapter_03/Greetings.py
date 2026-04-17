fa4834312e3a418bcbc092d183b6c92fd87ab1f9
lvazmecheng <262887731+lvazmecheng@users.noreply.github.com>
2026-04-17 13:13:54 +0500

    Finish exercise 3-2: created personalized greetings for each name in names list

---
+++ Chapter_03/Greetings.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+names = ['Connor', 'Jack', 'Zac', 'Tuan']
+print (f"Hello, {names[0]}!")
+print (f"Hello, {names[1]}!")
+print (f"Hello, {names[2]}!")
+print (f"Hello, {names[3]}!")
