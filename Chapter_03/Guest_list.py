b7493d77a3a0cd39abf464d3df42b8dcdbfddab2
lvazmecheng <262887731+lvazmecheng@users.noreply.github.com>
2026-04-17 18:18:39 +0500

    Finish exercise 3-4: created a guest list and print invites for each guest

---
+++ Chapter_03/Guest_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+guests = ['Connor', 'Jack', 'Amber', 'Zac']
+print(f"Dear {guests[0]}, you are invited to dinner.")
+print(f"Dear {guests[1]}, you are invited to dinner.")
+print(f"Dear {guests[2]}, you are invited to dinner.")
+print(f"Dear {guests[3]}, you are invited to dinner.")
