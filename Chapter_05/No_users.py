74248cda06da927fc0fc44091d3df202de4b99e7
lvazmecheng <262887731+lvazmecheng@users.noreply.github.com>
2026-04-18 16:14:01 +0500

    Finish 5-9: Added empty list handling to prevent silent logic skips

---
+++ Chapter_05/No_users.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+users = []
+if users != []:
+    for user in users:
+        if user == 'admin':
+            print(f"Hello admin, would you like to see a status report?")
+        else:
+            print(f"Hello {user}, thank you for logging in again.")
+else:
+    print(f"We need to find some users!")
