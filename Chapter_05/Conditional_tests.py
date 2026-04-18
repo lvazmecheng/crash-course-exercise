458e34c3e8720744255127857d3ef13620fa90e7
lvazmecheng <262887731+lvazmecheng@users.noreply.github.com>
2026-04-18 11:24:16 +0500

    Create Chapter 5 folder and finish exercise 5-1: equality and ineqaulity tests

---
+++ Chapter_05/Conditional_tests.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+dessert = "Tiramisu"
+print("Is dessert == 'Tiramisu'? I predict True.")
+print(dessert == "Tiramisu")
+print("\nIs dessert == 'Cheesecake'? I predict False.")
+print(dessert == "Cheesecake")
+
+favorite_fruit = "Pineapple"
+print("\nIs favorite_fruit == 'Pineapple'? I predict True.")
+print(favorite_fruit == "Pineapple")
+print("\nIs favorite_fruit == 'Apple'? I predict False.")
+print(favorite_fruit == "Apple")
+
+juice = "Orange"
+print("\nIs juice == 'Orange'? I predict True.")
+print(juice == "Orange")
+print("\nIs juice == 'Apple'? I predict False.")
+print(juice == "Apple")
+
+vegetable = "Garlic"
+print("\nIs vegetable == 'Garlic'? I predict True.")
+print(vegetable == "Garlic")
+print("\nIs vegetable == 'Onion'? I predict False.")
+print(vegetable == "Onion")
+
+car = "Honda"
+print("\nIs car == 'Honda'? I predict True.")
+print(car == "Honda")
+print("\nIs car == 'Toyota'? I predict False.")
+print(car == "Toyota")
