3d4a2873f3a925cb59ab706f9df4bd30dde03414
lvazmecheng <262887731+lvazmecheng@users.noreply.github.com>
2026-04-18 12:45:56 +0500

    Finish exercise 5-5: using if-elif-else for color matching

---
+++ Chapter_05/Alien_Colors_3.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You just earned 5 points.")
+elif alien_color == 'yellow':
+    print("You just earned 10 points.")
+elif alien_color == 'red':
+    print("You just earned 15 points.")
+else:
+    pass
+
+# Now the code will use alien_color = 'yellow' 
+
+alien_color = 'yellow'
+if alien_color == 'green':
+    print("You just earned 5 points.")
+elif alien_color == 'yellow':
+    print("You just earned 10 points.")
+elif alien_color == 'red':
+    print("You just earned 15 points.")
+else:
+    pass
+
+# Now the code will use alien_color = 'red'
+
+alien_color = 'red'
+if alien_color == 'green':
+    print("You just earned 5 points.")
+elif alien_color == 'yellow':
+    print("You just earned 10 points.")
+elif alien_color == 'red':
+    print("You just earned 15 points.")
+else:
+    pass
