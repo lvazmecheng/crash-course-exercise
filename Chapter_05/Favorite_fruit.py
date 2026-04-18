01601893377d41f52464c493c2e795b6de0135c9
lvazmecheng <262887731+lvazmecheng@users.noreply.github.com>
2026-04-18 12:59:45 +0500

    Finish exercise 5-7: using if statements to check list

---
+++ Chapter_05/Favorite_fruit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+favorite_fruits = ['pineapple', 'orange', 'guava']
+if 'pineapple' in favorite_fruits:
+    print("You really like pineapple!")
+if 'orange' in favorite_fruits:
+    print("You really like orange!")
+if 'guava' in favorite_fruits:  
+    print("You really like guava!")
+if 'banana' in favorite_fruits:
+    print("You really like banana!")
+if 'grape' in favorite_fruits:
+    print("You really like grape!")
