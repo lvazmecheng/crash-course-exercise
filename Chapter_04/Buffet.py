5aff95e158a8afcf6dcded54daa870f02f56974d
lvazmecheng <262887731+lvazmecheng@users.noreply.github.com>
2026-04-18 09:23:31 +0500

    Finish exercise 4-13: Implementing immutable tuples and variable re-assignment

---
+++ Chapter_04/Buffet.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+print("Original menu:")
+simple_foods = ('Burger', 'fries', 'salad', 'pasta', 'soup')
+for food in simple_foods:    
+    print(food)
+ 
+print("\nRevised menu:")
+revised_foods = ('Pizza', 'fries', 'salad', 'Pancakes', 'ice cream')
+for food in revised_foods:    
+    print(food)
