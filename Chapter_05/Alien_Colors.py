a4926ca6cdc346af3762f50785dc0fcf92b0a881
lvazmecheng <262887731+lvazmecheng@users.noreply.github.com>
2026-04-18 12:27:14 +0500

    Finish exercise 5-3: using conditional execution

---
+++ Chapter_05/Alien_Colors.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You just earned 5 points.")
+
+# Now this version of the program will not print anything because the condition 
+# is not met.
+alien_color = 'yellow'
+if alien_color == 'green':
+    print("You just earned 5 points.")
