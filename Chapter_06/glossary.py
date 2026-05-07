4f58d5bb026d98ff2dd7f049ae52f7805116f2e9
lvazmecheng <262887731+lvazmecheng@users.noreply.github.com>
2026-05-08 01:33:02 +0500

    Exercise 6-3: use a dictionary to print the meanings for words

---
+++ Chapter_06/glossary.py
@@ -0,0 +1,10 @@
+glossary = {'Interpreter': 'A program that executes code written in a high-level programming language',
+            'Variable': 'A named location in memory used to store data',
+            'List': 'An ordered collection of items',
+            'Loop': 'A control structure that repeats a block of code',
+            'Statement': 'A single line of code that performs an action'}
+print(f"Interpreter:\n {glossary['Interpreter']}")
+print(f"Variable:\n {glossary['Variable']}")
+print(f"List:\n {glossary['List']}")
+print(f"Loop:\n {glossary['Loop']}")
+print(f"Statement:\n {glossary['Statement']}")
