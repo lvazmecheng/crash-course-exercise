04b2e9916725d5ce27dfcd2110f1319dc540f921
lvazmecheng <262887731+lvazmecheng@users.noreply.github.com>
2026-05-08 00:54:33 +0500

    Complete 6-1: Implement first dictionary to store person attributes

---
+++ Chapter_06/person.py
@@ -0,0 +1,5 @@
+person = {'first_name': 'Jack', 'last_name': 'Frawley', 'age': '25', 'city': 'Melbourne'}
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
