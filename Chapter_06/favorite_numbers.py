1ec54d992fb99dd3be2a538d2c48208172d2eb66
lvazmecheng <262887731+lvazmecheng@users.noreply.github.com>
2026-05-08 01:14:25 +0500

    Exercise 6-2: used a dictionary to associate favorite numbers for five people

---
+++ Chapter_06/favorite_numbers.py
@@ -0,0 +1,6 @@
+favorite_num = {'Jack': 25, 'Connor': 32, 'Zac': 54, 'Amber': 71, 'Steph': 58}
+print(f"{favorite_num['Jack']} is Jack's favorite number.")
+print(f"{favorite_num['Connor']} is Connor's favorite number.")
+print(f"{favorite_num['Zac']} is Zac's favorite number.")
+print(f"{favorite_num['Amber']} is Amber's favorite number.")
+print(f"{favorite_num['Steph']} is Steph's favorite number.")
